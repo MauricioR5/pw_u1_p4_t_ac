9b535d260952c5fdb8565419d06c9472a51eb779
Alexander Cacuango <amcacuangoa@uce.edu.ec>
2023-05-15 14:52:50 -0500

    primera tarea

---
+++ src/main/java/com/example/demo/PwU1P4TAcApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwU1P4TAcApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwU1P4TAcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Tarea número 3");
+	}
+
+}
